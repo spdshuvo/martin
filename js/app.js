18d534e3762e6da0963eae707b0dff44af42e542
spdshuvo <shuvodev47@gmail.com>
2021-06-10 04:06:41 +0600

    js add

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(function (){
+    //sticky nav
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()>0){
+            $('#navber').addClass('stickyNav')
+        } else{
+            $('#navber').removeClass('stickyNav')
+        }
+    })
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()>600){
+            $(h)
+        }
+    })
+
+})
